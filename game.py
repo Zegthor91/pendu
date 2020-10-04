88ceee55f7483c0394515a04da2be36f1da3cc77
VianneyMI <vianney.mixtur@outlook.fr>
2020-10-04 11:47:10 +0200

    Added log of the games

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import player
 import data
+import libs
 class Game:
     def __init__(self,player,data,score=8):
         self.player=player
@@ -46,15 +47,30 @@ class Game:
                 # self.player.games+=1
                 # self.player.player_score+=self.game_score
                 self.player.update(self.endgame,self.game_score) #Update player statistics
+                self.update_gamesTable() #Update games dabatabase
                 print('Congratulations you won with a score of ', self.game_score, ' !')
         else:
             if(self.game_score==0):
                 self.endgame=-1
                 #self.player.games+=1
                 self.player.update(self.endgame,self.game_score) #Update player statistics
+                self.update_gamesTable() #Update games table
                 print('Sorry you lost !')
                 print('The word was : ') # Make optional eventually
                 self.data.reveal_word()
         #self.player.update(self.endgame,self.game_score) #Update player statistics
         return self.endgame
+
+    def update_gamesTable(self):
+        games=libs.pd.read_csv('database/games.csv', index_col=0)
+        newGame=libs.pd.DataFrame([[
+            self.player.username,
+            self.data.word,
+            self.endgame,
+            self.game_score
+        ]])
+        newGame.columns=games.columns
+        games=games.append(newGame, ignore_index=True)
+        games.index.name='gameID'
+        games.to_csv('database/games.csv')
 
